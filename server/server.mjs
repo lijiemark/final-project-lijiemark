be229e55cd7909504a31b1f5c72de22b63c1ff17
Lijie Xie <doomarktse@gmail.com>
2023-04-06 21:07:30 -0400

    prepare back-end deployment

--- server/server.mjs
+++ server/server.mjs
@@ -81,6 +81,15 @@ app.get('/logout', logout);
 
 
 const port = process.env.PORT || 3001;
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../my-app/build')));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../my-app/build', 'index.html'));
+//   });
+// }
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
